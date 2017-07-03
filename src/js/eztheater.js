0dea26b59676a3a8cc92c599f81863f54df0f0fe
Ryan Doyle <rcdoyle@mtu.edu>
2017-07-02 22:03:11 -0700

    colors and some tweaks on the rail

--- src/js/eztheater.js
+++ src/js/eztheater.js
@@ -1,4 +1,11 @@
 RAIL_SHIFT = '0px';
+MEDIA_DIR = '/secure/static/media';
+var main_index = {};
+
+$.getJSON(`${MEDIA_DIR}/index.json`, function(res){
+  main_index = res;
+  populate_rail();
+});
 
 $( document ).ready(function() {
    $('#load_overlay').remove()
@@ -8,12 +15,23 @@ $( document ).ready(function() {
 
 });
 
+function populate_rail(){
+  for(var i in main_index.movies){
+    let movie = main_index.movies[i];
+    $('.rail-bottom').append(`
+      <div class='rail-item'>
+        ${movie.name}
+      </div>
+    `);
+  }
+}
+
 function play_media(source){
-    $("#vid").css("display", "inline-block");
-    $("#tempmessage").css("display", "none");
-    $("#vid")[0].src = source;
-    $("#vid")[0].load();
-    $("#vid")[0].play();
+  $("#vid").css("display", "inline-block");
+  $("#tempmessage").css("display", "none");
+  $("#vid")[0].src = source;
+  $("#vid")[0].load();
+  $("#vid")[0].play();
 }
 
 function toggle_css(element, attr, v1, v2){
