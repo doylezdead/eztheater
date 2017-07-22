65b75718b5438d66a713f8d8b0a85674c6b1d94b
Ryan Doyle <rcdoyle@mtu.edu>
2017-07-21 23:04:59 -0700

    vids playin again

--- src/js/eztheater.js
+++ src/js/eztheater.js
@@ -2,34 +2,37 @@ RAIL_SHIFT = '0px';
 MEDIA_DIR = '/secure/static/media';
 var main_index = {};
 
-$.getJSON(`${MEDIA_DIR}/index.json`, function(res){
-  main_index = res;
-  populate_rail();
-});
 
 $( document ).ready(function() {
+   $.getJSON(`${MEDIA_DIR}/index.json`, function(res){
+     main_index = res;
+     populate_rail();
+   });
    $('#load_overlay').remove()
    // column_item = Handlebars.compile($.get('templates/column'));
    RAIL_SHIFT = "-" + $('.rail').css('width');
    $('#searchbox').focus();
-
 });
 
 function populate_rail(){
+  filter = $("#searchbox").val()
+  $('.rail-bottom').html("")
   for(var i in main_index.movies){
     let movie = main_index.movies[i];
-    $('.rail-bottom').append(`
-      <div class='rail-item'>
-        ${movie.name}
-      </div>
-    `);
+    if(movie.name.toLowerCase().indexOf(filter.toLowerCase()) >= 0){
+      $('.rail-bottom').append(`
+        <div class='rail-item', onclick="play_media(${i})">
+          ${movie.name}
+        </div>
+      `);
+    }
   }
 }
 
-function play_media(source){
+function play_media(i){
   $("#vid").css("display", "inline-block");
   $("#tempmessage").css("display", "none");
-  $("#vid")[0].src = source;
+  $("#vid")[0].src = "/secure/static/media/movies/" + main_index.movies[i].file;
   $("#vid")[0].load();
   $("#vid")[0].play();
 }
@@ -46,6 +49,7 @@ function toggle_css(element, attr, v1, v2){
 
 function search_update(){
   $('.rail').css('left', '0px');
+  populate_rail();
 }
 
 function select_burger(){
